87afd935ace58894e193f96d2235ecb0f8ad6b18
itsallvoodoo <chaddhobbs@gmail.com>
2014-02-05 13:29:57 -0500

    Got chart.js example running, need to customize

--- Beaglebone_Black/usr/local/bin/beaglebone-scripts/get_temp.py
+++ Beaglebone_Black/usr/local/bin/beaglebone-scripts/get_temp.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python2.7
+
 def fetch():
 	w1="/sys/bus/w1/devices/10-000800444018/w1_slave"
 	raw = open(w1, "r").read()
--- Beaglebone_Black/var/www/hvac/hvac.php
+++ Beaglebone_Black/var/www/hvac/hvac.php
@@ -1,8 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<script src="Chart.js"></script>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<script src="../js/Chart/Chart.js"></script>
+<meta name = "viewport" content = "initial-scale = 1, user-scalable = no">
+        <style>
+            canvas{
+            }
+        </style>
 <link rel="stylesheet" type="text/css" href="/style.css" />
 <title>Hobbs Home Automation Server</title>
 </head>
@@ -85,15 +89,10 @@
         	<p>This website is being served from a Beaglebone Black and is the same device that controls various functions around the house.</p>
         	<p>&nbsp;</p>
             <p>&nbsp;</p>
-            <canvas id="myChart" width="400" height="400"></canvas>
-
+            <canvas id="canvas" height="450" width="600"></canvas>
             <script>
-            
-                //Get the context of the canvas element we want to select
-                var ctx = document.getElementById("myChart").getContext("2d");
-                var myNewChart = new Chart(ctx).Line(data,options);
 
-                var data = {
+                var lineChartData = {
                     labels : ["January","February","March","April","May","June","July"],
                     datasets : [
                         {
@@ -111,92 +110,11 @@
                             data : [28,48,40,19,96,27,100]
                         }
                     ]
+                    
                 }
 
-                Line.defaults = {
-                    
-                    //Boolean - If we show the scale above the chart data           
-                    scaleOverlay : false,
-                    
-                    //Boolean - If we want to override with a hard coded scale
-                    scaleOverride : false,
-                    
-                    //** Required if scaleOverride is true **
-                    //Number - The number of steps in a hard coded scale
-                    scaleSteps : null,
-                    //Number - The value jump in the hard coded scale
-                    scaleStepWidth : null,
-                    //Number - The scale starting value
-                    scaleStartValue : null,
-
-                    //String - Colour of the scale line 
-                    scaleLineColor : "rgba(0,0,0,.1)",
-                    
-                    //Number - Pixel width of the scale line    
-                    scaleLineWidth : 1,
-
-                    //Boolean - Whether to show labels on the scale 
-                    scaleShowLabels : true,
-                    
-                    //Interpolated JS string - can access value
-                    scaleLabel : "<%=value%>",
-                    
-                    //String - Scale label font declaration for the scale label
-                    scaleFontFamily : "'Arial'",
-                    
-                    //Number - Scale label font size in pixels  
-                    scaleFontSize : 12,
-                    
-                    //String - Scale label font weight style    
-                    scaleFontStyle : "normal",
-                    
-                    //String - Scale label font colour  
-                    scaleFontColor : "#666",    
-                    
-                    ///Boolean - Whether grid lines are shown across the chart
-                    scaleShowGridLines : true,
-                    
-                    //String - Colour of the grid lines
-                    scaleGridLineColor : "rgba(0,0,0,.05)",
-                    
-                    //Number - Width of the grid lines
-                    scaleGridLineWidth : 1, 
-                    
-                    //Boolean - Whether the line is curved between points
-                    bezierCurve : true,
-                    
-                    //Boolean - Whether to show a dot for each point
-                    pointDot : true,
-                    
-                    //Number - Radius of each point dot in pixels
-                    pointDotRadius : 3,
-                    
-                    //Number - Pixel width of point dot stroke
-                    pointDotStrokeWidth : 1,
-                    
-                    //Boolean - Whether to show a stroke for datasets
-                    datasetStroke : true,
-                    
-                    //Number - Pixel width of dataset stroke
-                    datasetStrokeWidth : 2,
-                    
-                    //Boolean - Whether to fill the dataset with a colour
-                    datasetFill : true,
-                    
-                    //Boolean - Whether to animate the chart
-                    animation : true,
-
-                    //Number - Number of animation steps
-                    animationSteps : 60,
-                    
-                    //String - Animation easing effect
-                    animationEasing : "easeOutQuart",
-
-                    //Function - Fires when the animation is complete
-                    onAnimationComplete : null
-                }
-
-
+                var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+            
             </script>
 
 </div>
