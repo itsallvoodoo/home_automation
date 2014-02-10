76d468079ade02ef8ca23c59c0e5ee7904f3ff76
itsallvoodoo <chaddhobbs@gmail.com>
2014-02-09 22:31:36 -0500

    Cleaned up, time to rebuild

--- beaglebone_black/var/www/index.php
+++ beaglebone_black/var/www/index.php
@@ -1,10 +1,3 @@
-<?php
-    require '../includes/db_secure_connect.php';
-    require '../includes/functions.php';
- 
-    sec_session_start();
-?>
-
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -39,11 +32,11 @@
             <h3>Links</h3>
                     
             <ul>
-                <?php if (login_check($mysqli) == true) : ?>
-                    <li><a href="logout.php">Log out</a></li>
-                <?php else : ?>
-                    <li><a href="login.php">Log in</a></li>
-                <?php endif; ?>    
+
+                    <!--<li><a href="logout.php">Log out</a></li>
+
+                    <li><a href="login.php">Log in</a></li>-->
+   
                 <li><a href="#">Side2</a></li>
                 <li><a href="#">Side3</a></li>
                 <li><a href="#"></a></li>
@@ -60,24 +53,13 @@
         <div id="content_top"></div>
         <div id="content_main">
 
-            <?php if (login_check($mysqli) == true) : ?>
-                <p>Welcome <?php echo htmlentities($_SESSION['username']); ?>!</p>
-                <p>
-                    This is an example protected page.  To access this page, users
-                    must be logged in.  At some stage, we'll also check the role of
-                    the user, so pages will be able to determine the type of user
-                    authorised to access the page.
-                </p>
-
-            <p>Return to <a href="index.php">login page</a></p>
-        <?php else : ?>
         	<h2>Welcome to Chad's Home Automation Server </h2>
         	<p>&nbsp;</p>
            	<p>&nbsp;</p>
         	<p>This website is being served from a Beaglebone Black and is the same device that controls various functions around the house.</p>
         	<p>&nbsp;</p>
             <p>&nbsp;</p>
-        <?php endif; ?>
+
 
         </div>
         <div id="content_bottom"></div>
