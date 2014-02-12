e51cd0a65a14d5e14be024eb7f8dffcc7e88a53b
itsallvoodoo <chaddhobbs@gmail.com>
2014-02-11 23:13:32 -0500

    Added temperature to secure area

--- beaglebone_black/var/www/hvac/hvac.php
+++ beaglebone_black/var/www/hvac/hvac.php
@@ -19,7 +19,7 @@
 </head>
 
 <body>
-<?php include_once '../../includes/db_temp_connect.php'; ?> 
+
 <div id="container">
 		<div id="header">
         	<h1>Hobbs Home Automation Server</span></h1>
@@ -49,78 +49,88 @@
         <div id="content_main">
         	<h2>HVAC System Page</h2>
 
-            <?php
 
-                $sql="SELECT tempEntry FROM tempdata ORDER BY tempId DESC LIMIT 1";
-
-                $result = mysqli_query($mysqli,$sql);
-
-                echo "<table>";
-                echo "<tr>";
-                echo "<th>Current Temperature</th>";
-                echo "</tr>";
-
-                while($row = mysqli_fetch_array($result)) {
-                    echo "<tr>";
-                    echo "<td align=\"center\">" . $row['tempEntry'] . " F</td>";
-                    echo "</tr>";
-                    }
-                echo "</table>";
-
-                $sql="SELECT * FROM (SELECT * FROM tempdata ORDER BY tempId DESC LIMIT 10) AS `table` ORDER BY tempId ASC";
-
-                $result = mysqli_query($mysqli,$sql);
+            <?php if (login_check($mysqli) == true) :
                 
-            ?> 
-        	<p>&nbsp;</p>
-           	<p>&nbsp;</p>
-        	<p>&nbsp;</p>
-            <p>&nbsp;</p>
-            <canvas id="canvas" height="450" width="600"></canvas>
+                        include_once '../includes/db_temp_connect.php';
 
-            <script>
+                        $sql="SELECT tempEntry FROM tempdata ORDER BY tempId DESC LIMIT 1";
 
-                
+                        $result = mysqli_query($mysqli,$sql);
 
-                var lineChartData = {
-                    labels : [ <?php
+                        echo "
+                            <table>
+                            <tr>
+                            <th>Current Temperature</th>
+                            </tr>";
 
-                        $initial = True;
-
-                        while ($row = mysqli_fetch_array($result)) {
-                            if ($initial == False)  {
-                                echo ',';
+                        while($row = mysqli_fetch_array($result)) {
+                            echo "<tr>";
+                            echo "<td align=\"center\">" . $row['tempEntry'] . "F</td>";
+                            echo "</tr>";
                             }
-                            echo '"' . date('H:i', strtotime($row['tempTime'])) . '"';
-                            $initial = False;
-                        }
-                    ?>],
-                    datasets : [
-                        {
-                            fillColor : "rgba(220,220,220,0.5)",
-                            strokeColor : "rgba(220,220,220,1)",
-                            pointColor : "rgba(220,220,220,1)",
-                            pointStrokeColor : "#fff",
-                            data : [<?php
-                                        $initial = True;
-                                        $result = mysqli_query($mysqli,$sql);
-                                        while ($row = mysqli_fetch_array($result)) {
-                                            if ($initial == False)  {
-                                                echo ',';
-                                            }
-                                            echo round($row['tempEntry'],2);
-                                            $initial = False;
-                                        }
-                                        mysqli_close($con);  
-                            ?>]
-                        }
-                    ]
-                    
-                }
+                        echo "</table>";
 
-                var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
-            
-            </script>
+                        $sql="SELECT * FROM (SELECT * FROM tempdata ORDER BY tempId DESC LIMIT 10) AS `table` ORDER BY tempId ASC";
+
+                        $result = mysqli_query($mysqli,$sql);
+                
+                    ?> 
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
+                    <p>&nbsp;</p>
+                    <canvas id="canvas" height="450" width="600"></canvas>
+
+                    <script>
+
+                        
+
+                        var lineChartData = {
+                            labels : [ <?php
+
+                                $initial = True;
+
+                                while ($row = mysqli_fetch_array($result)) {
+                                    if ($initial == False)  {
+                                        echo ',';
+                                    }
+                                    echo '"' . date('H:i', strtotime($row['tempTime'])) . '"';
+                                    $initial = False;
+                                }
+                            ?>],
+                            datasets : [
+                                {
+                                    fillColor : "rgba(220,220,220,0.5)",
+                                    strokeColor : "rgba(220,220,220,1)",
+                                    pointColor : "rgba(220,220,220,1)",
+                                    pointStrokeColor : "#fff",
+                                    data : [<?php
+                                                $initial = True;
+                                                $result = mysqli_query($mysqli,$sql);
+                                                while ($row = mysqli_fetch_array($result)) {
+                                                    if ($initial == False)  {
+                                                        echo ',';
+                                                    }
+                                                    echo round($row['tempEntry'],2);
+                                                    $initial = False;
+                                                }
+                                                mysqli_close($con);  
+                                    ?>]
+                                }
+                            ]
+                            
+                        }
+
+                        var myLine = new Chart(document.getElementById("canvas").getContext("2d")).Line(lineChartData);
+                    
+                    </script>
+
+            <?php else : ?>
+                <p>
+                    <span class="error">You are not authorized to access this page.</span> Please <a href="../login.php">login</a>.
+                </p>
+            <?php endif; ?>                
 
 </div>
         <div id="content_bottom"></div>
---
+++ beaglebone_black/var/www/includes/db_temp_connect.php
@@ -0,0 +1,3 @@
+<?php
+include_once 'tempuser_config.php';   // As functions.php is not included
+$mysqli = new mysqli(HOST, USER, PASSWORD, DATABASE);
