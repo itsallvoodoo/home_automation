b9c5882b0d39eb894fb3c480a096e6a52dd37543
itsallvoodoo <chaddhobbs@gmail.com>
2014-02-15 23:31:42 -0500

    Formatting for future changes

--- beaglebone_black/var/www/leftmenu_main.php
+++ beaglebone_black/var/www/leftmenu_main.php
@@ -2,18 +2,21 @@
 
     include_once 'includes/db_connect.php';
     include_once 'includes/functions.php';
-    
-    echo 	"<h3>Links</h3>       
-    		<ul>";
+?>    
+    <h3>Links</h3>       
+    <ul>
+<?php
     if (login_check($mysqli) == true) :
-    	echo '        <li><a href="/includes/logout.php">Log out</a></li>';
-    else :
-    	echo '        <li><a href="/login.php">Log in</a></li>';
-	endif;
-    echo ' 	
-    	<li><a href="#">Side2</a></li>
-        <li><a href="#">Side3</a></li>
-        <li><a href="#"></a></li>
-		</ul>
-	';
 ?>
+        <li><a href="/includes/logout.php">Log out</a></li>
+<?php
+    else :
+?>
+        <li><a href="/login.php">Log in</a></li>
+<?php
+	endif;
+?>
+	<li><a href="#">Side2</a></li>
+    <li><a href="#">Side3</a></li>
+    <li><a href="#"></a></li>
+	</ul>
--- beaglebone_black/var/www/top_menu.php
+++ beaglebone_black/var/www/top_menu.php
@@ -1,10 +1,8 @@
 <?php
-	echo '
-		<ul>
-   		<li class="menuitem"><a href="/index.php">Home</a></li>
-    	<li class="menuitem"><a href="/access/access.php">Access Control</a></li>
-    	<li class="menuitem"><a href="/security/security.php">Security</a></li>
-    	<li class="menuitem"><a href="/hvac/hvac.php">HVAC</a></li>
-    	</ul>
-    ';
 ?>
+	<ul>
+		<li class="menuitem"><a href="/index.php">Home</a></li>
+		<li class="menuitem"><a href="/access/access.php">Access Control</a></li>
+		<li class="menuitem"><a href="/security/security.php">Security</a></li>
+		<li class="menuitem"><a href="/hvac/hvac.php">HVAC</a></li>
+	</ul>
