f59822bc6ef09bcafc17716a965e0788ef9e0280
itsallvoodoo <chaddhobbs@gmail.com>
2014-03-05 20:27:33 -0500

    Added dynamic menus to new layout

---
+++ beaglebone_black/var/www/context_menu.php
@@ -0,0 +1,6 @@
+<?php
+?>
+		<li class="active"><a href="/index.php">Home</a></li>
+		<li><a href="/access/access.php">Access Control</a></li>
+		<li><a href="/security/security.php">Security</a></li>
+		<li><a href="/hvac/hvac.php">HVAC</a></li>
---
+++ beaglebone_black/var/www/user_menu.php
@@ -0,0 +1,19 @@
+<?php
+
+    include_once 'includes/db_connect.php';
+    include_once 'includes/functions.php';
+    
+    if (login_check($mysqli) == true) :
+?>
+        <li><a href="/includes/logout.php">Log out</a></li>
+        <li><a href="#">Account</a></li>
+<?php
+    else :
+?>
+        <li><a href="/login.php">Log in</a></li>
+        <li><a href="#">Create Account</a></li>
+<?php
+	endif;
+?>
+	
+    <li><a href="#">Help</a></li>
