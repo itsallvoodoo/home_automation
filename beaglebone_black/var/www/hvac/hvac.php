dc2c64aa085221fcd84ae0af4c04dc1b376080f5
itsallvoodoo <chaddhobbs@gmail.com>
2014-03-06 15:43:00 -0500

    Further conversion

--- beaglebone_black/var/www/hvac/hvac.php
+++ beaglebone_black/var/www/hvac/hvac.php
@@ -1,66 +1,84 @@
 <?php
-    include_once '../includes/db_connect.php';
-    include_once '../includes/functions.php';
+  include_once '../includes/db_connect.php';
+  include_once '../includes/functions.php';
  
-    sec_session_start();
+  sec_session_start();
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<script>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+
+    <script>
     function testFunction() {
         alert("It works");
     }
-</script>
-<script src="../js/Chart/Chart.js"></script>
-<meta name = "viewport" content = "initial-scale = 1, user-scalable = no">
-        <style>
-            canvas{
-            }
-        </style>
-<link rel="stylesheet" type="text/css" href="/style.css" />
-<title>Hobbs Home Automation Server</title>
-</head>
+    </script>
+    <script src="../js/Chart/Chart.js"></script>
 
-<body>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This website is my development platform for web technologies and home automation">
+    <meta name="author" content="Chad Hobbs">
 
-<div id="container">
-		<div id="header">
-        	<h1>Hobbs Home Automation Server</span></h1>
-            <h2>HVAC Control</h2>
-        </div>   
-        
-        <div id="menu">
-        	<?php include '../top_menu.php'; ?>
+    <title>Hobbs Home Automation Server</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="../css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Specific stylesheet for my template -->
+    <link href="../css/mainsite.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Hobbs Home Automation Server</a>
         </div>
-        
-        <div id="leftmenu">
-
-        <div id="leftmenu_top"></div>
-
-				<div id="leftmenu_main">    
-                    <?php include '../leftmenu_main.php'; ?>
-                </div>
-                
-                
-              <div id="leftmenu_bottom"></div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <?php include '../user_menu.php'; ?>
+          </ul>
         </div>
-               
-		<div id="content">
-        
-        
-        <div id="content_top"></div>
-        <div id="content_main">
-        	<h2>HVAC System Page</h2>
-
-            <?php include 'tempData.php'; ?>          
-
-</div>
-        <div id="content_bottom"></div>
-            
-            <div id="footer"><h3>Footer Stuff</h3></div>
       </div>
-   </div>
-</body>
+    </div>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <?php include '../context_menu.php'; ?>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">HVAC Control</h1>
+
+          <?php include 'tempData.php'; ?>
+
+        </div>
+      </div>
+    </div>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/docs.min.js"></script>
+  </body>
 </html>
--- beaglebone_black/var/www/login.php
+++ beaglebone_black/var/www/login.php
@@ -11,43 +11,62 @@
     }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link rel="stylesheet" type="text/css" href="style.css" />
-    <script type="text/JavaScript" src="js/sha512.js"></script> 
-    <script type="text/JavaScript" src="js/forms.js"></script>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="This website is my development platform for web technologies and home automation">
+    <meta name="author" content="Chad Hobbs">
+
     <title>Hobbs Home Automation Server</title>
-</head>
 
-<body>
-<div id="container">
-    <div id="header">
-        <h1>Hobbs Home Automation Server</span></h1>
-        <h2>Login</h2>
-    </div>   
-        
-    <div id="menu">
-            <?php include 'top_menu.php'; ?>
-    </div>
-        
-    <div id="leftmenu">
+    <!-- Bootstrap core CSS -->
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
 
-        <div id="leftmenu_top"></div>
-        <div id="leftmenu_main">    
-            <?php include 'leftmenu_main.php'; ?>
+    <!-- Specific stylesheet for my template -->
+    <link href="/css/mainsite.css" rel="stylesheet">
+  </head>
+
+  <body>
+
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">Hobbs Home Automation Server</a>
         </div>
-            
-        <div id="leftmenu_bottom"></div>
+        <div class="navbar-collapse collapse">
+          <ul class="nav navbar-nav navbar-right">
+            <?php include 'user_menu.php'; ?>
+          </ul>
+        </div>
+      </div>
     </div>
-          
-    <div id="content">
-            
-        <div id="content_top"></div>
-        <div id="content_main">
 
-            <?php
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-sm-3 col-md-2 sidebar">
+          <ul class="nav nav-sidebar">
+            <?php include 'context_menu.php'; ?>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li></li>
+          </ul>
+          <ul class="nav nav-sidebar">
+            <li></li>
+          </ul>
+        </div>
+        <div class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+          <h1 class="page-header">Login</h1>
+
+          <?php
                 if (isset($_GET['error'])) {
                     echo '<p class="error">Error Logging In!</p>';
                 }
@@ -65,12 +84,15 @@
         <p>If you are done, please <a href="includes/logout.php">log out</a>.</p>
         <p>You are currently logged <?php echo $logged ?>.</p>
 
-
         </div>
-        <div id="content_bottom"></div>
-            
-        <div id="footer"><h3>Footer Stuff</h3></div>
+      </div>
     </div>
-</div>
-</body>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <script src="/js/docs.min.js"></script>
+  </body>
 </html>
