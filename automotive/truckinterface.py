e47d8c0f178e84e8297fac3c6dd01589190d8535
itsallvoodoo <chaddhobbs@gmail.com>
2014-03-12 21:06:57 -0400

    Tutorial method completed, will test tomorrow

--- automotive/truckinterface.py
+++ automotive/truckinterface.py
@@ -9,5 +9,18 @@
 
 
 # ------------- setup ----------------
-import serial
-ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1)
+
+import serial 	# This is the pyserial library, which enables communication via serial port
+
+
+ser = serial.Serial('/dev/ttyUSB0', 115200, timeout=1) #These are the parameters for my specific usb to serial adapter, (device,baud,timeout)
+
+
+
+# This method returns the speed at which the vehicle is travelling
+def get_speed():
+	ser.write("01 0D \r")
+	speed_hex = ser.readline().split(' ')
+	speed = float(int('0x'+speed_hex[3], 0 ))
+	print 'Speed: ', speed, 'mph'
+
